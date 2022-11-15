fc7a244fc116006361aac1a82c70af317f13a28b
LordStarkLK <2019cs059@stu.ucsc.lk>
2022-11-15 13:19:04 +0530

    project finished

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
-const express = require('express');
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const { response } = require("express");
 
 const app = express();
 
 const PORT = 5000;
 
-app.get("/",(res,req)=>{
-    res.render('hello');
-})
+app.get("/titles", (req, res) => {
+  axios("https://elakiri.com/")
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+
+      const results = $(".structItem-title")
+        .children()
+        .map(function (i, el) {
+          return $(this).text();
+        })
+        .toArray();
+      res.json(results);
+    })
+    .catch((err) => console.log(err));
+});
+
+// async function getTitles() {
+
+// }
 
 app.listen(PORT, () => console.log("server started " + PORT));
